cfcedf29f55c41e1d8522b7103e5999696d2a16c
Giles Bathgate <giles.bathgate@gmail.com>
2018-04-24 19:52:33 +0100

    Simple connection test

--- source/websocket.d
+++ source/websocket.d
@@ -1,9 +1,31 @@
 module websocket;
 
+import std.socket;
+
 class WebSocketServer
 {
+    static bool client()
+    {
+        auto domain = "localhost";
+        ushort port = 4000;
+        Socket sock = new TcpSocket(new InternetAddress(domain, port));
+        scope (exit)
+            sock.close();
+        return sock.isAlive();
+    }
+
+    static void server()
+    {
+        auto listener = new TcpSocket();
+        listener.bind(new InternetAddress(4000));
+        listener.listen(10);
+        auto client = listener.accept();
+    }
 }
 
 unittest {
-    assert(true);
+    import core.thread;
+    new Thread(&WebSocketServer.server).start();
+    auto connected = WebSocketServer.client();
+    assert(connected);
 }
